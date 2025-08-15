cd20a04093583632a03ba2b824857c8e3ec89ce1
Kuurama <74458092+Kuurama@users.noreply.github.com>
2025-08-15 06:29:55 +0200

    Update keymap.c

--- dYKNG/keymap.c
+++ dYKNG/keymap.c
@@ -20,7 +20,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_ESCAPE,      RALT(KC_EQUAL), RALT(KC_SCLN),  KC_BSPC,        KC_DELETE,      KC_ENTER,                                       KC_DELETE,      KC_GRAVE,       KC_CIRC,        TG(5),          KC_RIGHT_ALT,   TG(3),          
     KC_TAB,         KC_X,           KC_F,           KC_L,           KC_K,           KC_Q,                                           KC_P,           KC_G,           KC_O,           KC_U,           KC_COMMA,       KC_F12,         
     KC_LEFT_SHIFT,  KC_N,           KC_S,           KC_H,           KC_T,           KC_M,                                           KC_Y,           KC_C,           KC_A,           KC_E,           KC_I,           KC_SCLN,        
-    KC_LEFT_GUI,    MT(MOD_LALT, KC_B),KC_V,           KC_J,           KC_D,           MT(MOD_LALT, KC_Z),                                KC_QUOTE,       KC_W,           KC_DOT,         KC_SLASH,       KC_TRANSPARENT, KC_RIGHT_CTRL,  
+    KC_LEFT_GUI,    MT(MOD_LALT, KC_B),KC_V,           KC_J,           KC_D,           MT(MOD_LALT, KC_Z),                                KC_QUOTE,       KC_W,           KC_DOT,         KC_SLASH,       QK_REP, KC_RIGHT_CTRL,  
                                                     LT(1, KC_R),    MT(MOD_LCTL, KC_BSPC),                                MT(MOD_LSFT, KC_ENTER),LT(2, KC_SPACE)
   ),
   [1] = LAYOUT_voyager(
@@ -120,6 +120,16 @@ bool rgb_matrix_indicators_user(void) {
   return true;
 }
 
+bool remember_last_key_user(uint16_t keycode, keyrecord_t* record,
+                            uint8_t* remembered_mods) {
+  switch (keycode) {
+    case MT(MOD_LCTL, KC_BSPC):
+    case LT(2,KC_SPACE):
+    case MT(MOD_LSFT, KC_ENTER):
+      return false;
+  }
+  return true;
+}
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
